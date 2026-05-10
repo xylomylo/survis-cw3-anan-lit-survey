74c2ee8d2ca88419d14311d3a770c22adbe48e21
Anan Faiyaz <anan.faiyaz@gmail.com>
2026-05-10 18:58:26 +0100

    added categories to bib entries

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,5 +1,14 @@
 const userDefinedTagCategories = {
 	"type": {
 		"description": "type of the paper"
+	},
+	"category_a": {
+		"description": "Foundations"
+	},
+	"category_b": {
+		"description": "Wearable Sensor Integration and ML for HMI"
+	},
+	"category_c": {
+		"description": "sEMG-IMU Fusion and Cross-Subject Generalisation"
 	}
 }
