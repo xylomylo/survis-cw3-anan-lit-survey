af7383754f25d2c2c8d9a575fd5220b5425ae318
Anan Faiyaz <anan.faiyaz@gmail.com>
2026-05-10 19:17:52 +0100

    still fixing

--- src/data/authorized_tags.js
+++ src/data/authorized_tags.js
@@ -1,5 +1,14 @@
 const userDefinedAuthorizedTags = {
 	"type:system": {
 		"description": "paper describing a system"
+	},
+	"type:category_a": {
+		"description": "Foundations"
+	},
+	"type:category_b": {
+		"description": "Wearable Sensor Integration and ML for HMI"
+	},
+	"type:category_c": {
+		"description": "sEMG-IMU Fusion and Cross-Subject Generalisation"
 	}
 }
--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,14 +1,5 @@
 const userDefinedTagCategories = {
 	"type": {
 		"description": "type of the paper"
-	},
-	"category_a": {
-		"description": "Foundations"
-	},
-	"category_b": {
-		"description": "Wearable Sensor Integration and ML for HMI"
-	},
-	"category_c": {
-		"description": "sEMG-IMU Fusion and Cross-Subject Generalisation"
 	}
 }
